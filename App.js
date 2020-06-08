c989cdb1ff264e42265b6380381fe5fbf0ac348f
Sammy Robens-Paradise <srobensparadise@gmail.com>
2020-06-07 21:19:23 -0700

    added navs

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import 'react-native-gesture-handler';
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer,} from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux';
 // Imports: Screens
@@ -8,13 +9,16 @@ import Counter from './pages/Counter';
 // Imports: Redux Persist Persister
 import { store, persistor } from './store/store';
 // React Native: App
+const Stack  = createStackNavigator()
 export default function App() {
   return (
     // Redux: Global Store
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
       <NavigationContainer>
-        <Counter />
+        <Stack.Navigator>
+          <Stack.Screen name="Counter" component={Counter} />
+        </Stack.Navigator>
         </NavigationContainer>
       </PersistGate>
     </Provider>
